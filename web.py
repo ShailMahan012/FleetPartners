e42e79817e0618358458d44664ae02a2aa7726ae
ShailMurtaza <shailmurtaza@gmail.com>
2023-10-31 12:48:07 +0500

    use layout.html

--- web.py
+++ web.py
@@ -11,6 +11,12 @@ log.setLevel(logging.ERROR)
 def index():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("layout.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
 
