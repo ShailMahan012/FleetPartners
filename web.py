5a38dc90c23acd066c16c946755b161b325359d5
ShailMurtaza <shailmurtaza@gmail.com>
2023-11-14 19:21:02 +0500

    created contact page

--- web.py
+++ web.py
@@ -17,6 +17,11 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
 
